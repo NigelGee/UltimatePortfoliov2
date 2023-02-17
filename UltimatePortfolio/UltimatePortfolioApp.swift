43d70a9c5b21b4d80429fd07268e7103ea50404b
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2023-02-17 10:21:23 +0000

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Nigel Gee on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
